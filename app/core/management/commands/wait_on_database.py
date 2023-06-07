102d858a269a548dc7e0c5413679e0e178b9f232
sinnovah <sinnovah@gmail.com>
2023-06-07 13:38:08 +1200

    Create stub for wait on database command

---
+++ app/core/management/commands/wait_on_database.py
@@ -0,0 +1,15 @@
+"""
+Django command to pause execution until database is available.
+"""
+from django.core.management.base import BaseCommand
+
+
+class Command (BaseCommand):
+    """
+    Django command to pause execution until database is available.
+    Extends BaseCommand class from django.core.management.base.
+    """
+
+    def handle(self, *args, **options):
+        """Handle command stub, custom code follows"""
+        pass # Pass for now, will be implemented later
