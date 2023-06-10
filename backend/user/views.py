1cb2fcae2c93b4e8552d35206e4497a78a4242cd
sinnovah <sinnovah@gmail.com>
2023-06-11 07:13:40 +1200

    Add token view and URL

--- backend/user/views.py
+++ backend/user/views.py
@@ -2,8 +2,13 @@
 User API views.
 """
 from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-from user.serializers import UserSerializer
+from user.serializers import (
+    UserSerializer,
+    TokenSerializer
+)
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -14,3 +19,15 @@ class CreateUserView(generics.CreateAPIView):
 
     # Set DRF's serializer class to the custom user serializer
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """
+    Token API endpoint for user authentication [POST].
+    """
+    # Extends DRF's ObtainAuthToken view.
+
+    # Set DRF's serializer class to the custom token serializer
+    serializer_class = TokenSerializer
+    # Enable the browsable UI for the token API endpoint
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
