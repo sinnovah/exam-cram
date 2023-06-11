63a325a88219fe23a9ebb277588b4fc4819ce0cb
sinnovah <sinnovah@gmail.com>
2023-06-11 14:33:39 +1200

    Add view and URL to manage user

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,7 +1,7 @@
 """
 User API views.
 """
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -24,6 +24,7 @@ class CreateUserView(generics.CreateAPIView):
 class CreateTokenView(ObtainAuthToken):
     """
     Token API endpoint for user authentication [POST].
+    Requires valid login credentials to generate a token.
     """
     # Extends DRF's ObtainAuthToken view.
 
@@ -31,3 +32,26 @@ class CreateTokenView(ObtainAuthToken):
     serializer_class = TokenSerializer
     # Enable the browsable UI for the token API endpoint
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """
+    Endpoint to manage the authenticated user [GET, PUT, PATCH].
+    """
+    # Extends DRF's RetrieveUpdateAPIView.
+
+    # Set DRF's serializer class to the custom user serializer
+    serializer_class = UserSerializer
+
+    # Set the authentication and permission classes
+    # to the default DRF classes
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """
+        Retrieve and return the authenticated user
+        """
+
+        # Return the authenticated user
+        return self.request.user
