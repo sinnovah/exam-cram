629b540bd9dbca862c2185ce3cc447af8e08edf0
sinnovah <sinnovah@gmail.com>
2023-06-14 10:39:29 +1200

    Nest tag serializer inside topic

--- backend/topic/serializers.py
+++ backend/topic/serializers.py
@@ -9,10 +9,30 @@ from core.models import (
 )
 
 
+class TagSerializer(serializers.ModelSerializer):
+    """
+    Tag object.
+    """
+
+    class Meta:
+        """Meta class allows for validation rules for the data"""
+
+        # Associate the serializer with the tag model
+        model = Tag
+        # Fields to include in the tag API
+        fields = ['id', 'name']
+        # Make the id field read only
+        read_only_fields = ['id']
+
+
 class TopicSerializer(serializers.ModelSerializer):
     """
     Topic object.
     """
+
+    # List of tags (many=True) have a nested
+    # relationship to topic they are optional
+    tags = TagSerializer(many=True, required=False)
 
     class Meta:
         """Meta class allows for validation rules for the data"""
@@ -20,7 +40,7 @@ class TopicSerializer(serializers.ModelSerializer):
         # Associate the serializer with the topic model
         model = Topic
         # Fields to include in the topic API
-        fields = ['id', 'title', 'last_modified']
+        fields = ['id', 'title', 'last_modified', 'tags']
         # Make the id and last_modified fields read only
         read_only_fields = ['id', 'last_modified']
 
@@ -39,19 +59,3 @@ class TopicDetailSerializer(TopicSerializer):
 
         # Add additional fields to the topic detail method
         fields = TopicSerializer.Meta.fields + ['notes']
-
-
-class TagSerializer(serializers.ModelSerializer):
-    """
-    Tag object.
-    """
-
-    class Meta:
-        """Meta class allows for validation rules for the data"""
-
-        # Associate the serializer with the tag model
-        model = Tag
-        # Fields to include in the tag API
-        fields = ['id', 'name']
-        # Make the id field read only
-        read_only_fields = ['id']
