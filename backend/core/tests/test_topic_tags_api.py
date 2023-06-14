650feb452fe72e4348be06549f5f13a600b3e70e
sinnovah <85276154+sinnovah@users.noreply.github.com>
2023-06-14 13:32:43 +1200

    Creating topic tags implemented
    * Add unit tests for creating topic's tags
    
    * Remove unused imports for topic tags unit tests
    
    * Nest tag serializer inside topic
    
    * Override create topic so nested serializers can create

---
+++ backend/core/tests/test_topic_tags_api.py
@@ -0,0 +1,83 @@
+"""
+Unit tests for creating and assigning Tags to topics.
+"""
+from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+from core.tests.helpers import (
+    create_user,
+    create_tag,
+)
+from core.models import Topic
+
+
+# Topic list endpoint constant
+TOPICS_URL = reverse('topic:topic-list')
+
+
+class PrivateTopicTagsApiTests(TestCase):
+    """
+    Test suite for the topic tags (private access).
+    Authenticated requests.
+    """
+
+    def setUp(self):
+        """Set up the test suite."""
+
+        # Create a test client to make test http requests
+        self.client = APIClient()
+        # Create a test user
+        self.user = create_user()
+
+        # Authenticate the test user
+        self.client.force_authenticate(user=self.user)
+
+        # Topic data to send to endpoints
+        self.payload = {
+            'title': 'Test Topic',
+            'notes': 'Test notes for my topic',
+            'tags': [{'name': 'Test Tag'}, {'name': 'Test Tag 2'}]
+        }
+
+    def test_create_topic_with_existing_tags_successful(self):
+        """
+        Test creating a topic with existing tag does not duplicate the tag.
+        """
+
+        # Create an existing tag
+        existing_tag = create_tag(user=self.user, name='Test Tag')
+        # Post the payload with the same tag name as the existing tag
+        result = self.client.post(TOPICS_URL, self.payload, format='json')
+
+        # Check that the request was successful
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+
+        # Get the user's topics from the database
+        topics = Topic.objects.filter(user=self.user)
+
+        # Check that there is one topic
+        self.assertEqual(topics.count(), 1)
+
+        # Get the first and only topic from the queryset
+        topic = topics[0]
+
+        # Check that the topic has two tags
+        self.assertEqual(topic.tags.count(), 2)
+
+        # Check that the topic has the existing tag
+        # and not creating a duplicate tag
+        self.assertIn(existing_tag, topic.tags.all())
+
+        # Iterate over the payload's tags
+        for tag in self.payload['tags']:
+            # Check that the user's topic tag names
+            # exist and match the payload
+            self.assertTrue(
+                topic.tags.filter(
+                    user=self.user,
+                    name=tag['name']
+                ).exists()
+            )
--- backend/topic/serializers.py
+++ backend/topic/serializers.py
@@ -9,10 +9,30 @@ from core.models import (
 )
 
 
+class TagSerializer(serializers.ModelSerializer):
+    """
+    Tag object.
+    """
+
+    class Meta:
+        """Meta class allows for validation rules for the data"""
+
+        # Associate the serializer with the tag model
+        model = Tag
+        # Fields to include in the tag API
+        fields = ['id', 'name']
+        # Make the id field read only
+        read_only_fields = ['id']
+
+
 class TopicSerializer(serializers.ModelSerializer):
     """
     Topic object.
     """
+
+    # List of tags (many=True) have a nested
+    # relationship to topic they are optional
+    tags = TagSerializer(many=True, required=False)
 
     class Meta:
         """Meta class allows for validation rules for the data"""
@@ -20,9 +40,38 @@ class TopicSerializer(serializers.ModelSerializer):
         # Associate the serializer with the topic model
         model = Topic
         # Fields to include in the topic API
-        fields = ['id', 'title', 'last_modified']
+        fields = ['id', 'title', 'last_modified', 'tags']
         # Make the id and last_modified fields read only
         read_only_fields = ['id', 'last_modified']
+
+    def create(self, validated_data):
+        """
+        Override the create method to allow create for nested serializers.
+        """
+
+        # Remove the tags from the validated data
+        # Assign them to a tags variable
+        tags = validated_data.pop('tags', [])
+        # Create the topic without tags
+        topic = Topic.objects.create(**validated_data)
+        # Get the authenticated user
+        auth_user = self.context['request'].user
+
+        # Iterate over the popped tags
+        for tag in tags:
+            # Get or create the tag if it does not exist
+            # I.e., if the tag exists with the name and user passed in
+            # arguments, get it, else create it avoiding duplicate tags.
+            # **tag param allows for adding additional tag fields in the future
+            tag_object, created = Tag.objects.get_or_create(
+                user=auth_user,
+                **tag
+            )
+            # Add the tag to the topic
+            topic.tags.add(tag_object)
+
+        # Return the topic
+        return topic
 
 
 class TopicDetailSerializer(TopicSerializer):
@@ -39,19 +88,3 @@ class TopicDetailSerializer(TopicSerializer):
 
         # Add additional fields to the topic detail method
         fields = TopicSerializer.Meta.fields + ['notes']
-
-
-class TagSerializer(serializers.ModelSerializer):
-    """
-    Tag object.
-    """
-
-    class Meta:
-        """Meta class allows for validation rules for the data"""
-
-        # Associate the serializer with the tag model
-        model = Tag
-        # Fields to include in the tag API
-        fields = ['id', 'name']
-        # Make the id field read only
-        read_only_fields = ['id']
