ab044828c4d7c25a4a520bd907a3eee1f2071152
sinnovah <sinnovah@gmail.com>
2023-06-17 20:53:05 +1200

    Add unit tests: filter resources - assigned topics

--- backend/core/tests/test_resource_api.py
+++ backend/core/tests/test_resource_api.py
@@ -13,6 +13,7 @@ from core.models import Resource
 from core.tests.helpers import (
     create_user,
     create_resource,
+    create_topic,
     resource_details_url
 )
 
@@ -159,3 +160,64 @@ class PrivateResourceApiTests(TestCase):
 
         # Test that the resource has been deleted
         self.assertFalse(resource.exists())
+
+    def test_filter_resources_by_those_assigned_to_topics(self):
+        """
+        Test filtering resources by those assigned to topics.
+        """
+
+        # Create resources for the user
+        resource1 = create_resource(user=self.user, name='Resource 1')
+        resource2 = create_resource(user=self.user, name='Resource 2')
+        # Create a test topic for the user
+        topic = create_topic(user=self.user, name='Test Topic')
+        # Assign resource1 to the topic
+        topic.resources.add(resource1)
+
+        # Retrieve the resources assigned to the topic
+        # URL for the resources list, passing in the assigned_only query param
+        response = self.client.get(RESOURCES_URL, {'assigned_only': 1})
+
+        # Serialize the resources retrieved from the database
+        serializer1 = ResourceSerializer(resource1)
+        serializer2 = ResourceSerializer(resource2)
+
+        # Test that the get resources request was successful
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Test that there is 1 resource in the response
+        self.assertEqual(len(response.data), 1)
+        # Test that resource1 is in the response data
+        self.assertIn(serializer1.data, response.data)
+        # Test that resource2 is not in the response data
+        self.assertNotIn(serializer2.data, response.data)
+
+    def test_filter_resources_by_those_assigned_unique(self):
+        """
+        Test filtering resources by those assigned to
+        topics returns unique items.
+        """
+
+        # Create resources for the user
+        resource1 = create_resource(user=self.user, name='Resource 1')
+        resource2 = create_resource(user=self.user, name='Resource 2')
+        # Create a test topic for the user
+        topic1 = create_topic(user=self.user, name='Test Topic 1')
+        # Create a second test topic for the user
+        topic2 = create_topic(user=self.user, name='Test Topic 2')
+
+        # Assign resource1 to both topics
+        topic1.resources.add(resource1)
+        topic2.resources.add(resource1)
+
+        # Retrieve the resources assigned to topic1
+        # URL for the resources list, passing in the assigned_only query param
+        response = self.client.get(RESOURCES_URL, {'assigned_only': 1})
+
+        # Test that the get resources request was successful
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Test that there is 1 resource in the response
+        self.assertEqual(len(response.data), 1)
+        # Test that resource1 is in the response data
+        self.assertEqual(response.data[0]['name'], resource1.name)
+        # Test that resource2 is not in the response data
+        self.assertNotEqual(response.data[0]['name'], resource2.name)
